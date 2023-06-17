fc82078974d52eb6179fff5203cb02f35066a3a7
Anshul Purbey <89298423+anshulpurbey@users.noreply.github.com>
2023-06-17 13:58:24 +0530

    Update main-botInfo.js

--- plugins/main-botInfo.js
+++ plugins/main-botInfo.js
@@ -56,15 +56,9 @@ let infobt = `
 ▢ *${chats.length}* Total Chats
 
 *≡ OWNER*
-  *GURU*
-▢ Instagram :
-  • https://instagram.com/asli_guru69
+  *AnshulPurbey*
 ▢ GitHub :
-  • https://github.com/Guru322
-▢ Telegram : 
-  • t.me/i_want_to_be_isekaied (GURU) 
-▢ YouTube : 
-  • https://youtube.com/@Asliguru
+  • https://github.com/anshulpurbey
 
  *≡ S E R V E R*
 *🛑 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
