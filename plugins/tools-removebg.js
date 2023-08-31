7bcb8f50d6feac61e434dae3ca1b06528b77d156
Anshul Purbey <89298423+anshulpurbey@users.noreply.github.com>
2023-08-31 08:27:47 +0530

    Update tools-removebg.js

--- plugins/tools-removebg.js
+++ plugins/tools-removebg.js
@@ -20,7 +20,7 @@ const handler = async (m, { conn, text }) => {
     const response = await axios.post('https://api.remove.bg/v1.0/removebg', formData, {
       headers: {
         ...formData.getHeaders(),
-        'X-Api-Key': process.env.REMOVE_BG_API_KEY,
+        'X-Api-Key': "t3o6XtzmpPT1QSmpNxnd3Brb",
       },
       responseType: 'arraybuffer',
       encoding: null,
